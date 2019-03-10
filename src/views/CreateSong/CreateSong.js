a933c17a73fafeffe3828b0a8640aac7678fb155
Gideon <GideonHarmse0404@gmail.com>
2019-03-10 22:56:48 +0200

    .

--- src/views/CreateSong/CreateSong.js
+++ src/views/CreateSong/CreateSong.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const LayoutStyle = styled.div``;
+const LayoutStyle = styled.div`
+  display: grid;
+`;
 
 function CreateSong() {
   return <LayoutStyle>Create Song</LayoutStyle>;
